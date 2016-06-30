9b1d8abcfac2004cbb87acf57520287125537cb5
skotarov <skotarov@gmail.com>
2016-06-30 04:06:35 +0000

    m

--- www/admin/index.php
+++ www/admin/index.php
@@ -11,6 +11,8 @@ if($request_uri == 'index.php'  ||  substr($_SERVER['REQUEST_URI'],-1) == '/'){
 include __DIR__.'/../../lib/URLBase.php';
 define(URL_BASE, base_url());
 
+print_r(URLBASE);exit;
+
 if(isset($_SESSION['admin']['id']))
     header('Location: '.URL_BASE.'home/');
 else
